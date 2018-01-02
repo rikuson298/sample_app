aea8208347057eb18e9e58bfec13c5b0cef586e4
sumioku_r <rikuson298@gmail.com>
2018-01-02 19:35:05 +0900

    Complete advanced setup

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -22,7 +22,7 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
     assert_select "title", "Help | #{@base_title}"
   end
-
+  
   test "should get about" do
     get static_pages_about_url
     assert_response :success
